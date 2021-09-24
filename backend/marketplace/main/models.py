fd1fd412c70b9596d437db76059557cf9ec9358d
Kremon <kremoor@gmail.com>
2021-09-24 23:42:23 +0300

    откат до начального кастомного юзера
    возврат к изначальной идее через инфу в таблицах и незаморочного кастомного юзера

--- backend/marketplace/main/models.py
+++ backend/marketplace/main/models.py
@@ -1,14 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-
-""" STRUCTURE
+"""
 РОЛИ:
 детский дом
 воспитанник
 волонтёр
 ---------------
 модели
-Определяем новго пользователя
 
 участник
 воспитанник - от участника
@@ -17,50 +15,34 @@ from django.contrib.auth.models import AbstractUser
 площадка
 детский дом
 """
-class BasicUserManager(BaseException):
-    """
-    непонятно, как достать регион из уже сделанной модели
-    в душе не знаю, как это на самом деле должно рабоать, уверен, уже написанное развалится быстрее,
-    чем успеем запустить.
-    Можно считать, что набросал примерный макет базового юзера
-    """
-    def create_user(self, first_name, second_name, last_name, email, region, password=None):
 
-        user = self.model(
-            email=self.normalize_email(email),
-            first_name=first_name,
-            second_name=second_name,
-            last_name=last_name,
-            region=region
-        )
-
-        user.set_password(password)
-        user.save(user=self._db)
 
 class BasicUser(AbstractUser):
+    pass
+
+
+class Member(models.Model):
+    """
+    Человек
+    --------
+    ФИО
+    регион
+    """
     first_name = models.CharField(max_length=50)
     second_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
-    region = models.ForeignKey("Region", on_delete=models.PROTECT, db_index=True)
-    email = models.CharField(max_length=50, unique=True)
-    date_joined = models.DateTimeField(auto_now_add=True)
-
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['first_name', 'second_name', 'last_name', 'email', 'region',]
+    region = models.ForeignKey("Region", on_delete=models.PROTECT)
 
 
-
-
-'''
 class Child(models.Model):
     """
     Воспитанник детдома
     -----------------
-    человек - из таблицы MEMBERS
+    человек - из таблицы MEMEBERS
     детский дом
     """
     person = models.ForeignKey("Member", on_delete=models.PROTECT)
-    orphanage = models.Model("Orphanage")
+    orphanage = models.ForeignKey("Orphanage", on_delete=models.PROTECT)
 
 
 class Volunteer(models.Model):
@@ -73,7 +55,6 @@ class Volunteer(models.Model):
     """
     person = models.ForeignKey("Member", on_delete=models.PROTECT)
     active_events = models.ManyToManyField("Event")
-    finished_events = models.ManyToManyField("Event")
 
 
 class Event(models.Model):
@@ -92,8 +73,6 @@ class Event(models.Model):
     status = models.CharField(max_length=50)
     region = models.ForeignKey("Region", on_delete=models.PROTECT)
     organizer = models.ForeignKey("Organizer", on_delete=models.PROTECT)
-    orphanage = models.ForeignKey("Orphanage", on_delete=models.PROTECT)
-
 
 
 class Region(models.Model):
@@ -115,7 +94,6 @@ class Orphanage(models.Model):
     подконтрольные дети
     """
     title = models.CharField(max_length=100)
-    children = models.ManyToManyField("Child")
 
 
 class Organizer(models.Model):
@@ -125,5 +103,3 @@ class Organizer(models.Model):
     пока что не уверен в необходимости этой таблицы
     """
     title = models.CharField(max_length=100, db_index=True)
-'''
-

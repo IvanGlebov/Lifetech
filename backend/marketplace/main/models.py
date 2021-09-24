e8fc42b335afd6f1767745a19f71e7015fc477ab
Kremon <kremoor@gmail.com>
2021-09-24 22:28:14 +0300

    сделал набросок пользователя
    ничего не  понятно, ничего не знаю, помогимте

--- backend/marketplace/main/models.py
+++ backend/marketplace/main/models.py
@@ -1,11 +1,14 @@
 from django.db import models
-"""
+from django.contrib.auth.models import AbstractUser
+
+""" STRUCTURE
 РОЛИ:
 детский дом
 воспитанник
 волонтёр
 ---------------
 модели
+Определяем новго пользователя
 
 участник
 воспитанник - от участника
@@ -14,26 +17,46 @@ from django.db import models
 площадка
 детский дом
 """
-
-
-class Member(models.Model):
+class BasicUserManager(BaseException):
     """
-    Человек
-    --------
-    ФИО
-    регион
+    непонятно, как достать регион из уже сделанной модели
+    в душе не знаю, как это на самом деле должно рабоать, уверен, уже написанное развалится быстрее,
+    чем успеем запустить.
+    Можно считать, что набросал примерный макет базового юзера
     """
+    def create_user(self, first_name, second_name, last_name, email, region, password=none):
+
+        user = self.model(
+            email=self.normalize_email(email),
+            first_name=first_name,
+            second_name=second_name,
+            last_name=last_name,
+            region=region
+        )
+
+        user.set_password(password)
+        user.save(user=self._db)
+
+class BasicUser(AbstractUser):
     first_name = models.CharField(max_length=50)
     second_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
-    region = models.ForeignKey("Region", on_delete=models.PROTECT)
+    region = models.ForeignKey("Region", on_delete=models.PROTECT, db_index=True)
+    email = models.CharField(max_length=50, unique=True)
+    date_joined = models.DateTimeField(auto_now_add=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'second_name', 'last_name', 'email', 'region',]
 
 
+
+
+'''
 class Child(models.Model):
     """
     Воспитанник детдома
     -----------------
-    человек - из таблицы MEMEBERS
+    человек - из таблицы MEMBERS
     детский дом
     """
     person = models.ForeignKey("Member", on_delete=models.PROTECT)
@@ -52,7 +75,7 @@ class Volunteer(models.Model):
     active_events = models.ManyToManyField("Event")
     finished_events = models.ManyToManyField("Event")
 
-
+'''
 class Event(models.Model):
     """
     Мероприятие
@@ -70,6 +93,7 @@ class Event(models.Model):
     region = models.ForeignKey("Region", on_delete=models.PROTECT)
     organizer = models.ForeignKey("Organizer", on_delete=models.PROTECT)
     orphanage = models.ForeignKey("Orphanage", on_delete=models.PROTECT)
+
 
 
 class Region(models.Model):
@@ -101,3 +125,5 @@ class Organizer(models.Model):
     пока что не уверен в необходимости этой таблицы
     """
     title = models.CharField(max_length=100, db_index=True)
+
+
